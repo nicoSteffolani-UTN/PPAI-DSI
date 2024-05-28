76e96c6b97bddd06037cb45af474ac2b1738f2e2
Tici <ticigatica@gmail.com>
2024-05-28 19:32:39 -0300

    modelo varietal

---
+++ modelos/Varietal.py
@@ -0,0 +1,19 @@
+class Varietal:
+    def __init__(self, descripcion, porcent_comp):
+        self.descripcion = descripcion
+        self.porcent_comp = porcent_comp
+
+    def __str__(self):
+        return f"{self.descripcion} - {self.porcent_comp}"
+    
+    def getDescripcion(self):
+        return self.descripcion
+    
+    def setDescripcion(self, descripcion):    
+        self.descripcion = descripcion  
+
+    def getPorcenComp(self):
+        return self.porcent_comp
+    
+    def setPorcenComp(self, porcent_comp):
+        self.porcent_comp = porcent_comp
