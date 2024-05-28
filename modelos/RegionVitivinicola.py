e2edccfca19c5e59fc9065d23985947b11367e38
luciagiampieri <96505@sistemas.frc.utn.edu.ar>
2024-05-28 19:30:55 -0300

    Modelo RegionVitivinicola

---
+++ modelos/RegionVitivinicola.py
@@ -0,0 +1,25 @@
+class RegionVitivinicola: 
+    def __init__(self, descripcion, nombre, provincia):
+        self.nombre = descripcion
+        self.pais = nombre
+
+    def __str__(self):
+        return f"{self.descripcion} -- {self.nombre} -- {self.provincia}"
+    
+    def getDescripcion(self):
+        return self.descripcion
+    
+    def setDescripcion(self, descripcion):
+        self.descripcion = descripcion
+
+    def getNombre(self):
+        return self.nombre
+    
+    def setNombre(self, nombre):
+        self.nombre = nombre
+
+    def getProvincia(self):
+        return self.provincia
+    
+    def setProvincia(self, provincia):
+        self.provincia = provincia
