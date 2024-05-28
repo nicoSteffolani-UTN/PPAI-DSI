3c73161cf6dccc148682d124be9d20f0d8ecb6a5
Nico Steffolani <94196@sistemas.frc.utn.edu.ar>
2024-05-28 18:59:53 -0300

    carpeta modelos creada

---
+++ modelos/Pais.py
@@ -0,0 +1,13 @@
+class Pais:
+
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def __str__(self):
+        return self.nombre
+    
+    def getNombre(self):
+        return self.nombre
+    
+    def setNombre(self, nombre):
+        self.nombre = nombre
---
+++ modelos/Provincia.py
@@ -0,0 +1,22 @@
+
+
+class Provincia:
+
+    def __init__(self, nombre, pais):
+        self.nombre = nombre
+        self.pais = pais
+
+    def __str__(self):
+        return self.nombre
+    
+    def getNombre(self):
+        return self.nombre
+    
+    def setNombre(self, nombre):    
+        self.nombre = nombre
+
+    def getPais(self):
+        return self.pais
+    
+    def setPais(self, pais):
+        self.pais = pais
