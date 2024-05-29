b2e5b92066001224f9f01528914bbff53888412e
luciagiampieri <96505@sistemas.frc.utn.edu.ar>
2024-05-29 12:30:54 -0300

    eliminacion  lectorBodega

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 from flask import Flask, render_template
 from data.dataVinos.lectorPaises import leerPaises
-from data.dataVinos.lectorBodegas import leerBodegas
 import json
 
 app = Flask(__name__, template_folder='frontend/')
 
 @app.route('/')
 def index():
-    return json.dumps(leerBodegas())
+    return json.dumps(leerPaises())
 
 if __name__ == '__main__':
-    print(leerBodegas())
+    print(leerPaises())
     app.run(port=5000, debug=True)
 
