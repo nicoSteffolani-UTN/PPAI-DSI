dbe39fe069eecc97a664ed0c914fe75e30e59555
luciagiampieri <96505@sistemas.frc.utn.edu.ar>
2024-05-29 12:15:55 -0300

    lector de bodegas

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template
 from data.dataVinos.lectorPaises import leerPaises
+from data.dataVinos.lectorBodegas import leerBodegas
 import json
 
 app = Flask(__name__, template_folder='frontend/')
 
 @app.route('/')
 def index():
-    return json.dumps(leerPaises())
+    return json.dumps(leerBodegas())
 
 if __name__ == '__main__':
+    print(leerBodegas())
     app.run(port=5000, debug=True)
 
