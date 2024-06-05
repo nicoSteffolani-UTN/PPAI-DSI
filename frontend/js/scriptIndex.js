b6041346a10292e9c9b20b546f3d2b4a5aa7841c
Nico Steffolani <94196@sistemas.frc.utn.edu.ar>
2024-06-05 17:29:57 -0300

    version primera entrega sin exel ni habilitarPantalla

--- frontend/js/scriptIndex.js
+++ frontend/js/scriptIndex.js
@@ -1,6 +1,5 @@
 import { PantallaAdmReporteRanking } from './PantallaAdmReporteRanking.js';
 
-
 const btnOpcionRanking = document.getElementById('btn-opt-rank');
 
 const pantalla = new PantallaAdmReporteRanking();
