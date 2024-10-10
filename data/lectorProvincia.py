31c4f204e0391f052aa2a0caaff730ac659fa36b
barros98900 <98900@sistemas.frc.utn.ar>
2024-10-10 16:56:27 -0300

    lector provincia

---
+++ data/lectorProvincia.py
@@ -0,0 +1,34 @@
+import sqlite3
+import os 
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..')) 
+from modelos.Provincia import Provincia
+
+
+db_path = os.path.join(os.path.dirname(__file__), 'datos_prueba.db')
+conexion = sqlite3.connect(db_path)
+
+cursor = conexion.cursor()
+
+cursor.execute(
+    'SELECT * FROM PROVINCIAS'
+)
+
+provincias = cursor.fetchall()
+
+listaProvincias = []
+
+for provincia in provincias:
+    idProvincia = provincia[0]
+
+    cursor.execute(
+        'SELECT * FROM REGIONES WHERE PROVINCIA = ?', (int(idProvincia),)
+    )
+    regiones = cursor.fetchall()
+
+    provincia = Provincia(provincia[1], regiones)
+    listaProvincias.append(provincia)
+
+
+conexion.close()
