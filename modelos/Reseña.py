110b135f91fa81ffd9117c1085465004564f8401
Tici <ticigatica@gmail.com>
2024-10-10 16:08:44 -0300

    atributos privados reseña

--- modelos/Reseña.py
+++ modelos/Reseña.py
@@ -1,50 +1,50 @@
 class Reseña:
     def __init__(self, vino, id, comentario, puntaje, fechaReseña, esPremium):
-        self.id = id
-        self.comentario = comentario
-        self.puntaje = puntaje
-        self.fechaReseña = fechaReseña
-        self.esPremium = esPremium
-        self.vino = vino
+        self.__id = id
+        self.__comentario = comentario
+        self.__puntaje = puntaje
+        self.__fechaReseña = fechaReseña
+        self.__esPremium = esPremium
+        self.__vino = vino
 
     def __str__(self):
-        return f'{self.id} -- {self.comentario} -- {self.puntaje} -- {self.fechaReseña} -- {self.esPremium} -- {self.vino}'
+        return f'{self.__id} -- {self.__comentario} -- {self.__puntaje} -- {self.__fechaReseña} -- {self.__esPremium} -- {self.__vino}'
     
     def getId(self):
-        return self.id
+        return self.__id
 
     def setId(self, id):
-        self.id = id
+        self.__id = id
 
     def getComentario(self):
-        return self.comentario
+        return self.__comentario
 
     def setComentario(self, comentario):
-        self.comentario = comentario
+        self.__comentario = comentario
 
     def getPuntaje(self):
-        return self.puntaje
+        return self.__puntaje
 
     def setPuntaje(self, puntaje):
-        self.puntaje = puntaje
+        self.__puntaje = puntaje
 
     def getFechaReseña(self):
-        return self.fechaReseña
+        return self.__fechaReseña
 
     def setFechaReseña(self, fechaReseña):
-        self.fechaReseña = fechaReseña
+        self.__fechaReseña = fechaReseña
 
     def getEsPremium(self):
-        return self.esPremium
+        return self.__esPremium
 
     def setEsPremium(self, esPremium):
-        self.esPremium = esPremium
+        self.__esPremium = esPremium
 
     def getVino(self):
-        return self.vino
+        return self.__vino
 
     def setVino(self, vino):
-        self.vino = vino
+        self.__vino = vino
 
     def getDatosReseña(self):
         return {
@@ -53,12 +53,12 @@ class Reseña:
             "puntaje": self.getPuntaje(),
             "fechaReseña": self.getFechaReseña(),
             "esPremium": self.getEsPremium(),
-            "vino": self.vino.getDatosVino()
+            "vino": self.__vino.getDatosVino()
         }
     
     def validarFechaPeriodo(self, fechaDesde, fechaHasta):
-        return self.fechaReseña >= fechaDesde and self.fechaReseña <= fechaHasta
+        return self.__fechaReseña >= fechaDesde and self.__fechaReseña <= fechaHasta
     
     def sosDeVino(self, idVino):
-        if self.vino.id == idVino:
+        if self.__vino.id == idVino:
             return True
